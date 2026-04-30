45cb3ee236d7a4f95084bb14f32ca80878922cd5
1tue <161636469+1tuee@users.noreply.github.com>
2026-04-29 21:00:08 -0700

    Update vexmovement.py

--- vexmovement.py
+++ vexmovement.py
@@ -1,29 +1,28 @@
-from vex import * 
+from vex import *
 
-con = Controller() 
+con = Controller()
 
 brain = Brain()
-lm = Motor(ports.PORT1, GearSetting.RATIO_18_1, False)
-rm = Motor(Ports.PORT6, GearSetting.RATIO_18_1,True)
-
+lm = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+rm = Motor(Ports.PORT6, GearSetting.RATIO_18_1, True)
+drivetrain = Drivetrain(lm, rm, 200, 320, 130, MM)
+distance_sensor = Distance(Ports.PORT3)
 
 def main():
-	brain.screen.print("code init, working probably, leave the space inside the quotes blank to remove this")	
-
+    brain.screen.print("code init, working probably, leave the space ins")
+    avoidstuff()
 
 def avoidstuff():
-	while True:
-		diste = distance_sensor.distance(MM)
-		if distance_sensor.is_object_deteched() and diste < 200:
-			# bac kup and turn 
-			drivetrain.stop()
-			drivetrain.drive_for(REVERSE, 100, MM)
-			drivetrain.turn_for(RIGHT, 90, DEGREES)
-	 	else:
-			drivetrain.drive(FORWARD)
+    while True:
+        diste = distance_sensor.distance(MM)
+        if distance_sensor.is_object_detected() and diste < 200:
+            # bac kup and turn
+            drivetrain.stop()
+            drivetrain.drive_for(REVERSE, 100, MM)
+            drivetrain.turn_for(RIGHT, 90, DEGREES)
+        else:
+            drivetrain.drive(FORWARD)
 
-		wait(20, MSEC)
+    wait(20, MSEC)
 
-
-
-main() 
+main()
